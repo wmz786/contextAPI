96d3eab05afa402870ffed2f04374fc323382c7d
wmz <you@example.com>
2022-05-25 18:38:24 +0430

    accessing global state with context useing  .Consumer which works in both functional and classs based components

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,10 +1,28 @@
+import { ThemeContext } from "../contexts/ThemeContext";
+
+// accessing global state with context useing  .Consumer which works in both functional and classs based components
 const TodoList = () => {
   return (
-    <div className="ui list">
-      <p className="item">Plan the family trip</p>
-      <p className="item">Go for shoping for dinner</p>
-      <p className="item">Go for a walk</p>
-    </div>
+    <ThemeContext.Consumer>
+      {(context) => {
+        const { isDarkTheme, darkTheme, lightTheme } = context.values;
+        const theme = isDarkTheme ? darkTheme : lightTheme;
+        return (
+          <div
+            style={{
+              background: theme.background,
+              color: theme.text,
+              height: "140px",
+              textAlign: "center",
+            }}
+          >
+            <p className="item">Plan the family trip</p>
+            <p className="item">Go for shoping for dinner</p>
+            <p className="item">Go for a walk</p>
+          </div>
+        );
+      }}
+    </ThemeContext.Consumer>
   );
 };
 
