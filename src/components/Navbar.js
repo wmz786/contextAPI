8cf7287ff318232232cc9f3b92f0d5c06dd0a183
wmz <you@example.com>
2022-05-25 17:06:20 +0430

    use context for global state

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,21 @@
-import React from "react";
+import React, { useContext } from "react";
+import { ThemeContext } from "../contexts/ThemeContext";
 
 const Navbar = () => {
+  const values = useContext(ThemeContext);
+  const { isDarkTheme, darkTheme, lightTheme } = values;
+  const theme = isDarkTheme ? darkTheme : lightTheme;
   return (
-    <div>
-      <h2 className="ui centered header">Oak Academy</h2>
+    <div
+      style={{
+        background: theme.background,
+        color: theme.text,
+        hieght: "120px",
+      }}
+    >
+      <h2 style={{ textAlign: "center" }} className="ui centered header">
+        Oak Academy
+      </h2>
       <div className="ui three buttons">
         <button className="ui button">OverView</button>
         <button className="ui button">Contact</button>
