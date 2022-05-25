7c37eca151b01c85d47d30185f9ca61d02e942b9
wmz <you@example.com>
2022-05-25 18:37:48 +0430

    accessing global state with context useing useContext hook, it works just for functional based components

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,10 @@
 import React, { useContext } from "react";
 import { ThemeContext } from "../contexts/ThemeContext";
 
+
+// accessing global state with context useing useContext hook, it works just for functional based components
 const Navbar = () => {
-  const values = useContext(ThemeContext);
+  const { values } = useContext(ThemeContext);
   const { isDarkTheme, darkTheme, lightTheme } = values;
   const theme = isDarkTheme ? darkTheme : lightTheme;
   return (
@@ -13,9 +15,7 @@ const Navbar = () => {
         hieght: "120px",
       }}
     >
-      <h2 style={{ textAlign: "center" }} className="ui centered header">
-        Oak Academy
-      </h2>
+      <h2 style={{ textAlign: "center" }}>Oak Academy</h2>
       <div className="ui three buttons">
         <button className="ui button">OverView</button>
         <button className="ui button">Contact</button>
@@ -26,3 +26,4 @@ const Navbar = () => {
 };
 
 export default Navbar;
+  
