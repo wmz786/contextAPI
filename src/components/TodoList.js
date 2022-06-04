20cca1e3815795395488d64c4e4eaced6f582b87
wmz <you@example.com>
2022-06-04 05:41:37 +0430

    add removeTodo context action for deleting the todos

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,10 +1,22 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { ThemeContext } from "../contexts/ThemeContext";
 import { TodoListContext } from "../contexts/TodoListContext";
 
 // accessing global state with context useing  .Consumer which works in both functional and classs based components
 const TodoList = () => {
-  const { todos } = useContext(TodoListContext);
+  const { todos, addTodo, removeTodo } = useContext(TodoListContext);
+  const [todo, setTodo] = useState("");
+  const handleChange = (e) => {
+    setTodo(e.target.value);
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    addTodo(todo);
+  };
+  const handleRemoveTodo = (e) => {
+    const id = e.target.id;
+    removeTodo(id);
+  };
   return (
     <ThemeContext.Consumer>
       {(context) => {
@@ -15,19 +27,32 @@ const TodoList = () => {
             style={{
               background: theme.background,
               color: theme.text,
-              height: "140px",
+              // height: "140px",
               textAlign: "center",
             }}
           >
             {todos.length ? (
               todos.map((todo) => (
-                <p key={todo.id} className="item">
+                <p
+                  id={todo.id}
+                  onClick={handleRemoveTodo}
+                  key={todo.id}
+                  className="item"
+                >
                   {todo.text}
                 </p>
               ))
             ) : (
               <div>Your todos list is empty</div>
             )}
+            <form onSubmit={handleSubmit}>
+              <input type="text" onChange={handleChange} />
+              <input
+                type="submit"
+                value="Add New Todo"
+                className="ui primary button"
+              />
+            </form>
           </div>
         );
       }}
--- src/contexts/TodoListContext.js
+++ src/contexts/TodoListContext.js
@@ -3,13 +3,23 @@ import { createContext, useState } from "react";
 export const TodoListContext = createContext();
 
 const TodoListContextProvider = ({ children }) => {
-  const [todos] = useState([
+  const [todos, setTodos] = useState([
     { text: "Plan the family trip", id: 1 },
     { text: "Go for shoping for dinner", id: 2 },
     { text: "recitation of quran kari", id: 4 },
   ]);
+  const addTodo = (todo) => {
+    setTodos([...todos, { text: todo, id: Math.random() }]);
+  };
+  const removeTodo = (id) => {
+    setTodos(
+      todos.filter((todo) => {
+        return todo.id !== Number(id);
+      })
+    );
+  };
   return (
-    <TodoListContext.Provider value={{ todos }}>
+    <TodoListContext.Provider value={{ todos, addTodo, removeTodo }}>
       {children}
     </TodoListContext.Provider>
   );
